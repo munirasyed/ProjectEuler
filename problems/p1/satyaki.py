17e2509fae73f67c3c2a9b87e3d6c082aa50460b
Satyaki Sikdar <ssikdar@nd.edu>
2018-12-18 20:51:21 -0600

    added problem statement

--- problems/p1/satyaki.py
+++ problems/p1/satyaki.py
@@ -1,3 +1,12 @@
+'''
+Multiples of 3 and 5
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+
 def sum_nk(n, k):
     '''
     Finds the sum of numbers less than n that are a multiple of k
