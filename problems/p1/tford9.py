f27d0a94c870da7b3d688718e9b1003273c85996
Trenton Ford <tford9@lsu.edu>
2018-12-24 14:11:02 -0600

    more shaving p1

--- problems/p1/tford9.py
+++ problems/p1/tford9.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-x = 3; y = 5; n = 1000;
-xy = x*y;
-n -= 1;
+def main():
+	x = 3; y = 5; n = 1000;
+	xy = x*y;
+	n -= 1;
 
-upper_bound_x_multiplier  = int(n/x);
-result  = sum ([ x*(idx) for idx in range(1,upper_bound_x_multiplier+1)]);
-upper_bound_y_multiplier  = int(n/y);
-result += sum ([ y*(idx) for idx in range(1,upper_bound_y_multiplier+1)]);
-upper_bound_xy_multiplier = int(n/(xy));
-result -= sum ([ xy*(idx) for idx in range(1,upper_bound_xy_multiplier+1)]);
+	upper_bound_x_multiplier  = n//x;
+	result  = sum ([ x*(idx) for idx in range(1,upper_bound_x_multiplier+1)]);
+	upper_bound_y_multiplier  = n//y;
+	result += sum ([ y*(idx) for idx in range(1,upper_bound_y_multiplier+1)]);
+	upper_bound_xy_multiplier = n//(xy);
+	result -= sum ([ xy*(idx) for idx in range(1,upper_bound_xy_multiplier+1)]);
 
-print(result);
-
-
+	print(result);
 
 
+if "__name__" == "__main__":
+	main()
