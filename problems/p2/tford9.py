4107ad0bb07051bad77e0d7c058265870478c090
Trenton Ford <tford9@lsu.edu>
2018-12-24 15:47:25 -0600

    p2 shaving

--- problems/p2/tford9.py
+++ problems/p2/tford9.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# closed-form Fibonnaci function
-def cf_fibonnaci(n):
-    sqrt5 = 5**(0.5);
-    return int((1/sqrt5)*(((1+sqrt5)/2)**n - ((1-sqrt5)/2)**n))
-
 def main():
-	N = 4*(10**6) #upper-bound 4M
+	N = 4000000;
+	sqrt5 = 5**(0.5);
+	golden_ratio = (1+sqrt5)/2;
 
 	result = 0
 	for n in range(3,100,3):
-	    f_n = cf_fibonnaci(n);
+	    f_n = ((golden_ratio**n-(1-golden_ratio)**n) // sqrt5);
 	    if (f_n < N):
 	        result += f_n;
 	    else:
 	        break;
-	print(result)
+
+	print(result);
 
 
 if "__name__" == "__main__":
