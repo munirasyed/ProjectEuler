0e83860a3b2039a94a7882753b0ac50a17215476
Trenton Ford <tford9@lsu.edu>
2018-12-24 13:58:56 -0600

    shaving

--- problems/p1/tford9.py
+++ problems/p1/tford9.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import math as m, numpy as np;
-
 x = 3; y = 5; n = 1000;
+xy = x*y;
+n -= 1;
 
-upper_bound_x_multiplier, x_multiplier_remainder  = divmod(n,x);
-upper_bound_y_multiplier, y_multiplier_remainder = divmod(n,y);
-upper_bound_xy_multiplier, xy_multiplier_remainder = divmod(n,(x*y));
-
-if not x_multiplier_remainder:
-    upper_bound_x_multiplier = upper_bound_x_multiplier - 1;
-if not y_multiplier_remainder:
-    upper_bound_y_multiplier = upper_bound_y_multiplier - 1;
-if not xy_multiplier_remainder:
-    upper_bound_xy_multiplier = upper_bound_xy_multiplier - 1;
-
-result = sum ([ x*(idx) for idx in range(upper_bound_x_multiplier+1) ]);
-result += sum ([ y*(idx) for idx in range(upper_bound_y_multiplier+1) ]);
-result -= sum ([ x*y*(idx) for idx in range(upper_bound_xy_multiplier+1) ]);
+upper_bound_x_multiplier  = int(n/x);
+result  = sum ([ x*(idx) for idx in range(1,upper_bound_x_multiplier+1)]);
+upper_bound_y_multiplier  = int(n/y);
+result += sum ([ y*(idx) for idx in range(1,upper_bound_y_multiplier+1)]);
+upper_bound_xy_multiplier = int(n/(xy));
+result -= sum ([ xy*(idx) for idx in range(1,upper_bound_xy_multiplier+1)]);
 
 print(result);
 
